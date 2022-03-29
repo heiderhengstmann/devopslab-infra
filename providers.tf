f41fb64e5d7980c1b1e8aa801dde375e83abbd71
Heider Leandro Massu Hengstmann <heidermassu@gmail.com>
2022-03-28 23:41:02 -0300

    correcao caminho

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file(".\gcpkey.json")
+  credentials = file(".\\gcpkey.json")
 
   project = "devoplab"
   region  = "us-west1"
